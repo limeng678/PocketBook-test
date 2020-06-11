03aa05aba611760b16023252a2f12f36d7abd989
lm <710450868@qq.com>
2020-06-11 23:24:36 +0800

    active-class路由激活

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,16 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
      <Icon name="money"/>
       记账
     </router-link>
     |
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
     |
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -28,18 +28,23 @@
     box-shadow: 0 0 3px rgba(0,0,0,0.25);
     flex-direction: row;
     font-size:12px;
-    > .item{
-      padding:2px 0;
+    > .item {
+      padding: 2px 0;
       width: 33.33333%;
       display: flex;
       justify-content: center;
       align-items: center;
       flex-direction: column;
-      .icon{
-        width:32px;
-        height:32px;
+
+      .icon {
+        width: 32px;
+        height: 32px;
       }
     }
-  }
+      > .item.selected {
+        color: #42b983;
+      }
+    }
+
 
 </style>
