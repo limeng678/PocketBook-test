0333719c6af5d77aa9e1856fe18b2f1e4595dae5
lm <710450868@qq.com>
2020-06-16 10:40:07 +0800

    使用全局tagList

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -4,4 +4,22 @@ type RecordItem = {
   type: string;
   amount: number;
   createdAt?: Date;
+
+
+}
+type Tag={
+  id: string;
+  name: string;
+}
+type TagListModel={
+  data: Tag[];
+  fetch: () => Tag[];
+  create: (name: string) => 'success'|'duplicated';
+  update: (id: string,name: string) => 'success'|'not found'|'duplicated';
+  remove: (id: string) => boolean;
+  save: () => void;
+}
+interface Window {
+  tagList: Tag[];
+
 }
