de2d239605f7a4083515d073700b08923bf3cf27
lm <710450868@qq.com>
2020-06-16 20:43:58 +0800

    Money.vue使用Vuex

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -8,7 +8,6 @@
                 @update:value="onUpdateNotes"/>
     </div>
     <Tags />
-
   </Layout>
 </template>
 
@@ -19,22 +18,28 @@
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component} from 'vue-property-decorator';
-  import store from '@/store/index2';
+
 
   @Component({
     components: {Tags,  FormItem, Types, NumberPad},
+    computed: {
+      recordList () {
+        return this.$store.state.recordList;
+      }
+    }
   })
   export default class Money extends Vue {
-    recordList= store.recordList;
     record: RecordItem = {
       tags: [], notes: '', type: '-', amount: 0
     };
-
+    created(){
+      this.$store.commit('fetchRecords')
+    }
     onUpdateNotes(value: string) {
       this.record.notes = value;
     }
     saveRecord() {
-       store.createRecord(this.record);
+       this.$store.commit('createRecord',this.record);
     }
   }
 </script>
