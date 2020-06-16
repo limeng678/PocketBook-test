969be09edab35030f397cb39f9ecfa6669b9f008
lm <710450868@qq.com>
2020-06-16 22:42:40 +0800

    EditLabel.vue使用Vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,16 +5,26 @@ import createId from '@/lib/createId';
 
 Vue.use(Vuex)
 
+type RootState={
+  recordList: RecordItem[];
+  tagList: Tag[];
+  currentTag?: Tag;
+}
+
 const store= new Vuex.Store({
   state: {
-   recordList:[] as RecordItem[],
-    tagList:[] as Tag[],
-  },
-  mutations:{
-    fetchRecords(state) {
-      state.recordList=JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
+   recordList:[] ,
+    tagList:[] ,
+    currentTag: undefined
+  } as RootState,
+  mutations: {
+    setCurrentTag(state, id: string) {
+      state.currentTag = state.tagList.filter(t => t.id === id)[0];
     },
-    createRecord(state,record){
+    fetchRecords(state) {
+      state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
+    },
+    createRecord(state, record) {
       const record2: RecordItem = clone(record);
       record2.createdAt = new Date();
       state.recordList.push(record2);
@@ -25,26 +35,26 @@ const store= new Vuex.Store({
       window.localStorage.setItem('recordList',
         JSON.stringify(state.recordList));
     },
-    fetchTags(state){
-      return state.tagList=JSON.parse(window.localStorage
-        .getItem('tagList') || '[]') ;
+    fetchTags(state) {
+      return state.tagList = JSON.parse(window.localStorage
+        .getItem('tagList') || '[]');
     },
-    createTag(state,name: string) {
+    createTag(state, name: string) {
       const names = state.tagList.map(item => item.name);
       if (names.indexOf(name) >= 0) {
         window.alert('标签名重复了');
-        return 'duplicated';
+
       }
       const id = createId().toString();
       state.tagList.push({id, name: name});
       store.commit('saveTags');
       window.alert('添加成功');
-      return 'success';
+
     },
     saveTags(state) {
       window.localStorage.setItem('tagList',
         JSON.stringify(state.tagList));
-    }
+    },
   }
 });
 
